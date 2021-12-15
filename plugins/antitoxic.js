e14da6f30c0821857e0f664ea43907acedd8c1fc
rifqikhanif <94571775+rifqikhanif@users.noreply.github.com>
2021-12-15 07:29:04 +0700

    Update antitoxic.js

--- plugins/antitoxic.js
+++ plugins/antitoxic.js
@@ -7,7 +7,7 @@ conn.sendMessage(m.chat, ara, MessageType.audio, {quoted: m, mimetype: 'audio/mp
 
 handler.customPrefix = ['/(a(su|nj(([ie])ng|([ie])r)?)|me?me?k|ko?nto?l|ba?bi|fu?ck|ta(e|i)k|bangsat|g([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i'] 
 handler.tags = ['audio'] 
-handler.command = /^(/(a(su|nj(([ie])ng|([ie])r)?)|me?me?k|ko?nto?l|ba?bi|fu?ck|ta(e|i)k|bangsat|g([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i)$/i
+handler.command = /(a(su|nj(([ie])ng|([ie])r)?)|me?me?k|ko?nto?l|ba?bi|fu?ck|ta(e|i)k|bangsat|g([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i
 
 handler.owner = false
 handler.mods = false 
