8c7f0be53ec066fc7396e60870354e79fc9bfc56
rifqikhanif <94571775+rifqikhanif@users.noreply.github.com>
2021-12-15 07:32:51 +0700

    Update syg.js

--- plugins/syg.js
+++ plugins/syg.js
@@ -1,11 +1,11 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m) => {
-let pale = fs.readFileSync('./audio/pale.mp3') 
+let pale = fs.readFileSync('./audio/Sayang.mp3') 
 conn.sendMessage(m.chat, pale, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 }
 
-handler.customPrefix = /^(pale)?$/i
+handler.customPrefix = /^(sayang)?$/i
 handler.command = new RegExp
 
 module.exports = handler
